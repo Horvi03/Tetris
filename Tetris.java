6e09a60e6c6969efe3fda893621baa725b0148e9
Horvi03 <146196760+Horvi03@users.noreply.github.com>
2025-03-06 23:26:27 +0100

    Add files via upload

---
+++ Tetris.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Tetris {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tetris");
+        frame.setDefaultCloseOperation(JFrane.EXIT_ON_CLOSE);
+        frame.setSize(250, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+}
